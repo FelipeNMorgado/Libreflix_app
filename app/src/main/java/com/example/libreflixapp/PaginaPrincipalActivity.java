1b4091041f257a6812364e4700dc43a3fcdb284c
arvvv <avvv@cesar.school>
2024-11-14 23:23:10 -0300

    Adicionando tela inicial e transição de tela

--- app/src/main/java/com/example/libreflixapp/PaginaPrincipalActivity.java
+++ app/src/main/java/com/example/libreflixapp/PaginaPrincipalActivity.java
@@ -1,8 +1,6 @@
 package com.example.libreflixapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class PaginaPrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pagina_principal2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
-
-        new Handler().postDelayed(() -> {
-
-            Intent intent = new Intent(MainActivity.this, PaginaPrincipalActivity.class);
-            startActivity(intent);
-
-            finish();
-        }, 1000);
     }
 }
